b63548ce8f17ae607f0fc7f6317ee45681502d65
EthanNad02 <ethan.nad02@gmail.com>
2026-05-28 15:38:29 +0200

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-    </main>
-    <footer>
-        <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> CoinMalin - Projet BTS SIO</p>
-        </div>
-    </footer>
-</body>
-</html>
